d645c778391f236bfe921f98f48c3a6b24a14f0f
hvp <harivishnuprasadk@gmail.com>
2023-02-02 23:11:24 +0530

    proj commit

---
+++ CrudDemo/Controllers/ExceptionHandleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers;
+
+[ApiController]
+public class ExceptionHandleController : ControllerBase
+{
+
+    [Route("exception")]
+    [HttpGet]
+    public IActionResult Get(int a)
+    {
+        try
+        {
+            if (a == 0)
+            {
+                throw new NullReferenceException("Invalid input");
+            }
+
+            if (a == 1)
+            {
+                throw new Exception();
+            }
+
+        }
+        catch (NullReferenceException e)
+        {
+            return StatusCode(400, "Invalid Input");
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "int server err");
+
+        }
+
+
+        return Content("works");
+    }
+
+
+    [Route("test")]
+    [HttpPost]
+    public string Post()
+    {
+        return "post";
+    }
+
+    [Route("add")]
+    [HttpGet]
+    public int Add(int x, int y)
+    {
+        return (x + y);
+    }
+}
+
