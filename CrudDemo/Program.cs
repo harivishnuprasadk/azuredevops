1b2f23c49676ddfa46a9b6e421c8029264cb0f04
hvp <harivishnuprasadk@gmail.com>
2023-02-03 20:47:16 +0530

    added key vault integration

--- CrudDemo/Program.cs
+++ CrudDemo/Program.cs
@@ -2,17 +2,24 @@
 using Microsoft.EntityFrameworkCore;
 using CrudDemo.Repository;
 using CrudDemo.Models;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<IssueDbContext>(
-    o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
+var keyVaultEndpoint = new Uri(builder.Configuration["VaultKey"]);
+var secretClient = new SecretClient(keyVaultEndpoint, new DefaultAzureCredential());
+KeyVaultSecret kvs = secretClient.GetSecret("SqlServer");
+builder.Services.AddDbContext<IssueDbContext>(o=>o.UseSqlServer(kvs.Value));
+
+//builder.Services.AddDbContext<IssueDbContext>(
+//    o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddScoped<IRepository<Issue, int>,IssuesRepository>();
 var app = builder.Build();
 
